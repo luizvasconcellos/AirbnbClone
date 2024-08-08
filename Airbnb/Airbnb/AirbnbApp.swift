8824c39cbdcb05a31d924a2a3ffba56f7017491d
Luiz Vasconcellos <>
2024-08-08 12:28:05 +0100

    Create Project

---
+++ Airbnb/Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Luiz Vasconcellos on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
