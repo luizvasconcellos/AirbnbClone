781eadb202cac4a3c759aa5ae7b17c5aaf414d20
Luiz Vasconcellos <>
2024-08-08 14:03:04 +0100

    Create SearchAndFilterBarView

--- Airbnb/Airbnb/Views/Explore/ExploreView.swift
+++ Airbnb/Airbnb/Views/Explore/ExploreView.swift
@@ -11,6 +11,8 @@ struct ExploreView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
+                SearchAndFilterBarView()
+                
                 LazyVStack(spacing: 32) {
                     ForEach(0 ... 10, id: \.self) { listing in
                         ListingItemView()
---
+++ Airbnb/Airbnb/Views/Explore/SearchAndFilterBarView.swift
@@ -0,0 +1,45 @@
+//
+//  SearchAndFilterBarView.swift
+//  Airbnb
+//
+//  Created by Luiz Vasconcellos on 08/08/24.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBarView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            VStack(alignment: .leading, spacing: 2) {
+                Text("where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywere - Any Week - Add guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            })
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SearchAndFilterBarView()
+}
